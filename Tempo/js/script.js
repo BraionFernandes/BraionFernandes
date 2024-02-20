e25ce18949e7da5dca88055a19a3c25223a6a96d
BraionFernandes <132958791+BraionFernandes@users.noreply.github.com>
2024-02-20 13:21:23 -0300

    ....

---
+++ Tempo/js/script.js
@@ -0,0 +1,80 @@
+const apikey="b51fe3da165a019d2e32946960f50a78";
+
+const cityInput=document.querySelector("#cityName");
+const searchBtn=document.querySelector("#search");
+
+const cityElement=document.querySelector("#city");
+const tempElement=document.querySelector("#temperature span");
+const descElement=document.querySelector("#description");
+const weatherIconElement=document.querySelector("#weather-icon");
+const countryElement=document.querySelector("#pais");
+const umidityElement=document.querySelector("#umidity span");
+const windElement=document.querySelector("#wind span");
+const msgUndefined=document.getElementById("msgUndefined");
+
+const weatherContainer=document.querySelector("#weather-data");
+
+const cityContainer=document.getElementById("cityContainer");
+const citySugerido=[...document.querySelectorAll(".citySugerido")];
+
+const msgLoading=document.getElementById("msgLoading");
+
+const getWeatherData=async(city)=>{
+    const apiWeatherURL=`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apikey}&lang=pt_br`;
+
+    const res=await fetch(apiWeatherURL)
+    const data=await res.json()
+
+    return data;
+}
+const showWeatherData=async(city)=>{
+    const data=await getWeatherData(city);
+    if(data.name===undefined){
+        error();
+        msgLoading.classList.add("hide");
+    }else{
+    msgUndefined.classList.add("hide");
+    cityElement.innerHTML=data.name;
+    tempElement.innerHTML=parseInt(data.main.temp);
+    descElement.innerHTML=data.weather[0].description;
+    weatherIconElement.setAttribute("src",`https://openweathermap.org/img/wn/${data.weather[0].icon}.png`);
+    countryElement.setAttribute("src",`https://flagsapi.com/${data.sys.country}/flat/64.png`);
+    umidityElement.innerHTML=`${data.main.humidity}%`;
+    windElement.innerText=`${data.wind.speed}km/h`;
+    
+    msgLoading.classList.add("hide");
+    weatherContainer.classList.remove("hide");
+    }
+}
+searchBtn.addEventListener("click",()=>{
+    msgUndefined.classList.add("hide");
+    cityContainer.classList.add("hide");
+    weatherContainer.classList.add("hide");
+    msgLoading.classList.remove("hide");
+    e.preventDefault()
+    const city=cityInput.value;
+    showWeatherData(city);
+    cityInput.value="";
+})
+cityInput.addEventListener("keyup",(e)=>{
+    if(e.code === "Enter"){
+        msgUndefined.classList.add("hide");
+        cityContainer.classList.add("hide");
+        weatherContainer.classList.add("hide");
+        msgLoading.classList.remove("hide");
+        const city =e.target.value
+        showWeatherData(city);
+        cityInput.value="";
+    }
+})
+citySugerido.forEach((el)=>{
+    el.addEventListener("click",()=>{
+        msgLoading.classList.remove("hide");
+        showWeatherData(el.textContent);
+        cityContainer.classList.add("hide");
+    })
+})
+function error(){
+    weatherContainer.classList.add("hide");
+    msgUndefined.classList.remove("hide");
+}
